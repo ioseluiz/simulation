814dd0f5ce90732617d6a03642adb552acb8dc7c
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2023-06-08 15:20:28 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,195 @@
 import pygame
-import time
-from utils import calc_total_distance, calc_speed
+from datetime import datetime, timedelta
 
-class Truck:
-    def __init__(self, cord_x, cord_y):
-        self.cord_x = cord_x
-        self.cord_y = cord_y
-        DEFAULT_IMAGE_SIZE = (80, 80)
-        self.truck = pygame.image.load('truck.png')
-        self.truck = pygame.transform.scale(self.truck, DEFAULT_IMAGE_SIZE)
-        self.truck.convert()
-        self.truck_rect = self.truck.get_rect()
-        self.truck_rect.center = cord_x, cord_y
-        self.path = [(self.cord_x, self.cord_y),(600,self.cord_y),(600,100)]
-        self.distance = calc_total_distance(self.path)
-        self.speed = calc_speed(self.distance, 0,5)
+from utils import check_batch_time, check_trip_ended
+# Initialize pygame
+pygame.init()
 
-    
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (0,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+# Batch Plant
 
 
-    
-    def move(self,speed_x,speed_y):
-        return self.truck_rect.move(speed_x, speed_y)
-    
-class Plant:
-    def __init__(self, cord_x, cord_y):
-        self.cord_x = cord_x
-        self.cord_y = cord_y
-        DEFAULT_IMAGE_SIZE = (80, 80)
-        self.batch = pygame.image.load('batch_plant.png')
-        self.batch = pygame.transform.scale(self.batch, DEFAULT_IMAGE_SIZE)
-        self.batch.convert()
-        self.batch_rect = self.batch.get_rect()
-        self.batch_rect.center = cord_x, cord_y
+# Destination Lift - Equipment
+EQUIPMENT = 1
+if EQUIPMENT == 1:
+    placing_equipment = [
+        {
+            'id':1,
+            'name': 'Telebelt 01',
+            'x': 550,
+            'y': 250,
+        }
+    ]
+    path = [(120,250),(350, 250),(530,250)]
+elif EQUIPMENT == 2:
+    placing_equipment = [
+        {
+            'id':1,
+            'name': 'Telebelt 01',
+            'x': 550,
+            'y': 50,
+        },
+        {
+            'id':2,
+            'name': 'Telebelt 02',
+            'x': 550,
+            'y': 550,
+        },
+
+    ]
+    #path = [(120,270),(350, 270),(),(530,270)]
+elif EQUIPMENT == 3:
+    placing_equipment = [
+        {
+            'id':1,
+            'name': 'Telebelt 01',
+            'x': 550,
+            'y': 50,
+        },
+        {
+            'id':2,
+            'name': 'Telebelt 02',
+            'x': 550,
+            'y': 250,
+        },
+        {
+            'id':3,
+            'name': 'Telebelt 03',
+            'x': 550,
+            'y': 550,
+        },
+
+    ]
 
 
-def main():
-    # pygame setup
-    pygame.init()
 
-    # Define Colors
-    BLACK = (0,0,0)
-    WHITE = (255,255,255)
-    GREEN = (0,255,255)
-    RED = (255,0,0)
-    BLUE = (0,0,255)
+# Truck DATA
+truck_trips = [
+    {
+        'id': 1,
+        'second_batch_datetime': datetime(2013,11,14,6,29),
+        'arrive_time': datetime(2013,11,14,6,37),
+        'start_time': datetime(2013,11,14,6,39),
+        'finish_time': datetime(2013,11,14,6,40),
+    },
+    {
+    'id': 2,
+        'second_batch_datetime': datetime(2013,11,14,6,33),
+        'arrive_time': datetime(2013,11,14,6,40),
+        'start_time': datetime(2013,11,14,6,44),
+        'finish_time': datetime(2013,11,14,6,49),
+    },
 
-    # Set the size for the image
-    DEFAULT_IMAGE_SIZE = (80, 80)
+]
 
-    #text = "DATE: "
-    screen = pygame.display.set_mode((800,500))
-    pygame.display.set_caption("Concrete Truck Simulation")
-    sysfont = pygame.font.get_default_font()
-    print('system font :', sysfont)
+# Date information
+start_datetime = datetime(2013,11,14,6,27)
+end_datetime = datetime(2013,11,14,6,55)
+duration = end_datetime - start_datetime
+duration = duration.total_seconds() // 60
+actual_datetime = start_datetime 
 
-    clock = pygame.time.Clock()
-    running = True
+# System Font
+system_font = pygame.font.SysFont("calibri", 34)
 
-    coord_x = 400
-    coord_y = 200
+#Window Size
+WINDOW_WIDTH = 600
+WINDOW_HEIGTH = 600
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH))
+pygame.display.set_caption("Concrete Truck Simulation")
 
-    # Speed in which rect will move
-    speed_x = 3
-    speed_y = 0
+FPS = 1
+clock = pygame.time.Clock()
+t = 0
+started_trips = []
 
-    #t0 = time.time()
-    
-    #print('time needed for Font creation: ',time.time()-t0)
+# Define classes
+class Truck(pygame.sprite.Sprite):
+    """A simple class to represent a Concrete Truck"""
+    def __init__(self, id,x, y):
+        super().__init__()
+        self.id = id
+        self.image = pygame.image.load("truck.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.speed_x = 0
+        self.speed_y = 0
 
-    # truck = pygame.image.load('truck.png')
-    # truck = pygame.transform.scale(truck, DEFAULT_IMAGE_SIZE)
-    # truck.convert()
-    # truck_rect = truck.get_rect()
-    # truck_rect.center = coord_x, coord_y
-    truck_1 = Truck(coord_x, coord_y)
-    font = pygame.font.SysFont(None, 48)
-    time = clock.get_time()
-    img = font.render('Time taken: {} ms'.format(time), True, (0, 0, 0))
-    
+    def update(self, speed_x, speed_y):
+        "Update and move the truck"
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        self.x += self.speed_x
+        self.y += self.speed_y
 
-   
+class Equipment():
+    """A simple class to represent concrete placement equipment"""
+    def __init__(self, id,name,x, y):
+        self.id = id
+        self.name = name
+        self.x = x
+        self.y = y
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        time = pygame.time.get_ticks()
-        font = pygame.font.SysFont(None, 48)
-        img = font.render('Time taken: {} s'.format(time/1000), True, (0, 0, 0))
+class Plant():
+    """A simple class to represent Main Batch Plant"""
+    def __init__(self, id, name,x,y):
+        self.id = id
+        self.name = name
+        self.x = x
+        self.y = y
         
 
+# Game Main loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        if (coord_x > 720 or coord_x < 0):
-            speed_x *= -1
+    # Create Text
+    system_text = system_font.render(f"DATE: {actual_datetime}", True, BLACK)
+    system_text_rect  = system_text.get_rect()
+    system_text_rect.topleft = (0,0)
 
-        coord_x += speed_x
+    # Fill the display surface to cover old images
+    display_surface.fill(WHITE)
 
-        # fill the screen with a color to wipe away anything from the last frame
-        screen.fill(WHITE)
-        screen.blit(img, (20,20))
+    # Check batch time to create trucks
+    started_trips += check_batch_time(truck_trips, actual_datetime)
+    check_trip_ended(started_trips, actual_datetime)
+    print(started_trips)
+   
 
-        #screen.blit(truck, truck_rect)
+    # Instantiate Batch Plant class
+    plant = Plant(1, 'Main Batch Plant', 50, 250)
+    pygame.draw.rect(display_surface, RED, (plant.x, plant.y, 40,40))
 
-        # RENDER YOUR GAME HERE
-        #pygame.draw.rect(screen, RED, (coord_x, coord_y, 80, 80))
-        truck_1.truck_rect = truck_1.move(speed_x,speed_y)
-        screen.blit(truck_1.truck, truck_1.truck_rect)
-        pygame.display.update()
+    # Instantiate equipment class
+    for equip in placing_equipment:
+        telebelt = Equipment(equip['id'], equip['name'], equip['x'], equip['y'])
+        pygame.draw.rect(display_surface, BLUE,(telebelt.x,telebelt.y, 40,40))
 
-        # flip() the display to put your work on screen
-        #pygame.display.flip()
+    # Blit assets
+    display_surface.blit(system_text, system_text_rect)
 
-        # limits FPS to 60
-        clock.tick(60)
+    if len(started_trips) > 0:
+        for trip in started_trips:
+            truck = Truck(trip['id'], path[0][0], path[0][1])
+            display_surface.blit(truck.image, truck.rect)
 
-    pygame.quit()
+    # Update Screen
+    pygame.display.update()
 
+    clock.tick(FPS)
+    t += 1
+    actual_datetime += timedelta(minutes=1)
+    if t > duration:
+        break
 
-if __name__ == '__main__':
-    main()
+# End game
+pygame.quit()
--- utils.py
+++ utils.py
@@ -1,44 +1,13 @@
+def check_batch_time(trips, actual_datetime):
+    trips_initiated = []
+    for trip in trips:
+        if trip['second_batch_datetime'] == actual_datetime:
+            trips_initiated.append(trip)
+    return trips_initiated
 
 
-def calc_speed(distance, start_time, end_time):
-    return (distance/(end_time - start_time))
-
-def calc_delta_x(point_1, point_2):
-    coor_x_1 = point_1[0]
-    coor_x_2 = point_2[0]
-    return (coor_x_2 - coor_x_1)
-
-def calc_delta_y(point_1, point_2):
-    coor_y_1 = point_1[1]
-    coor_y_2 = point_2[1]
-    return (coor_y_2 - coor_y_1)
-
-def calc_total_distance(points):
-    total = 0
-    for i in range(1,len(points)):
-        previous_point = points[i-1]
-        actual_point = points[i]
-        total += calc_distance_points(previous_point, actual_point)
-    return total
-
-def calc_distance_points(point_1, point_2):
-    # Coordinates of point 1
-    coor_x_1 = point_1[0]
-    coor_y_1 = point_1[1]
-    # Coordinates of point 2
-    coor_x_2 = point_2[0]
-    coor_y_2 = point_2[1]
-
-    distance = ((coor_x_2 - coor_x_1)**2 + (coor_y_2 - coor_y_1)**2)**0.5
-    print(distance)
-    return distance
-
-
-if __name__ == '__main__':
-    list_points = [(0,0),(2,2),(3,5)]
-    print(calc_total_distance(list_points))
-    
-
-
-
-    
+def check_trip_ended(started_trips, actual_datetime):
+    for trip in started_trips:
+        if trip['finish_time'] == actual_datetime:
+            print(f"trip con id: {trip['id']} eliminado...")
+            started_trips.remove(trip)
